179f265a5eb43bb25558af180baa4bfc5eae96bc
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-12-07 15:41:11 +0100

    Updating endpoint /products and creating new dynamic /:name

--- backend/App.js
+++ backend/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Int32 } = require('mongodb');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Dodaj endpoint do obsługi żądania GET na głównym adresie
 app.get('/', (req, res) => {
@@ -20,14 +20,28 @@ const mongoose = require("mongoose");
 const server = '127.0.0.1:27017';
 const database = 'KioskMC';
 
-const travelSchema = new mongoose.Schema({
+const kioskProductsSchema = new mongoose.Schema({
+    id: Number,
     name: String,
     image: String,
-    // inne pola według potrzeb
+    price: Number,
+    cuisine: String,
+    minQuantity: Number,
+    maxQuantity: Number,
+    ingredients: [
+        {
+            name: String,
+            quantity: Number,
+            isEditable: Boolean,
+            minQuantity: Number,
+            maxQuantity: Number
+        }
+    ]
 });
 
 // Tworzysz model dla kolekcji na podstawie schematu
-const TravelModel = mongoose.model('countries', travelSchema);
+const KioskProductsModel = mongoose.model('products', kioskProductsSchema);
+
 
 
 class Database {
@@ -49,20 +63,40 @@ class Database {
         // Endpoint do pobrania danych z kolekcji i wyświetlenia ich
         app.get('/products', async (req, res) => {
             try {
-                // Pobierasz wszystkie dokumenty z kolekcji
-                const documents = await TravelModel.find({});
+                // Pobierasz tylko wybrane pola z kolekcji i wykluczasz pole "_id"
+                const documents = await KioskProductsModel.find({}, 'name price image cuisine -_id');
 
                 // Zwracasz pobrane dokumenty jako JSON w odpowiedzi do klienta
                 res.json(documents);
             } catch (error) {
                 console.error('Error retrieving data from collection:', error);
                 res.status(500).send('Internal Server Error');
-            } finally {
-                // Zamykanie połączenia po zakończeniu operacji
-                mongoose.connection.close();
+            }
+        });
+
+        // Dodajesz dodatkowy endpoint do pobrania pojedynczego produktu na podstawie nazwy
+        app.get('/products/:name', async (req, res) => {
+            const productName = req.params.name;
+
+            try {
+                // Pobierasz pojedynczy produkt na podstawie nazwy
+                const product = await KioskProductsModel.findOne({ name: productName }, 'name price image cuisine -_id');
+
+                // Sprawdzasz, czy produkt istnieje
+                if (!product) {
+                    return res.status(404).json({ error: 'Product not found' });
+                }
+
+                // Zwracasz pojedynczy produkt jako JSON w odpowiedzi do klienta
+                res.json(product);
+            } catch (error) {
+                console.error('Error retrieving data from collection:', error);
+                res.status(500).send('Internal Server Error');
             }
         });
     }
+
+
 
 }
 
